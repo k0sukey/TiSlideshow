e77e38a109ce3651ad98f56b58e11624724c384a
Kosuke Isobe <naughtydog0721@gmail.com>
2012-03-28 13:46:23 +0900

    init commit

---
+++ Resources/app.js
@@ -0,0 +1,19 @@
+Ti.UI.setBackgroundColor('#000');
+
+var app = {};
+app.slideshow = require('slideshow').Slideshow();
+
+var window = Ti.UI.createWindow({
+	fullscreen: true
+});
+
+var slideshowView = app.slideshow.createSlideshowView({
+	idleTimerDisabled: false,
+	width: window.width,
+	height: window.height,
+	duration: 2000,
+	images: [ 'images/image00.jpg', 'images/image01.jpg', 'images/image02.jpg' ]
+});
+window.add(slideshowView);
+
+window.open();
